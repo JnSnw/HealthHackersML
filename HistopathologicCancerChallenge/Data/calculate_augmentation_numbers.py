c58487b079fbaf5815f604e3e73c9effdc85d298
Thomas Koller <thomas-koller@gmx.net>
2019-02-07 21:19:33 +0100

    Calculate augmentation numbers

---
+++ HistopathologicCancerChallenge/Data/calculate_augmentation_numbers.py
@@ -0,0 +1,28 @@
+import pandas as pd
+df_targets = pd.DataFrame({'data': y, 'label':y})    
+
+group_num = 1
+df_targets["group"] = -1
+
+for idx, item in df_targets.iterrows(): 
+    
+    df_targets.loc[idx, "group"] = group_num
+    if (idx + 1) % 100 == 0:
+        group_num = group_num + 1
+
+
+df_targets_grouped = df_targets.groupby('group')
+
+for num, df_batches in df_targets_grouped:
+    
+    df_labels = df_batches.groupby('label').count()
+    
+    num_positive = df_labels.query('label == 1').reset_index().loc[0, 'data']
+    num_negative = df_labels.query('label == 0').reset_index().loc[0, 'data']
+
+    if num_negative > num_positive:
+        print('Batch {}: Create {} positive augmentations'.format(num, num_negative - num_positive))
+    elif num_positive > num_negative:
+        print('Batch {}: Create {} positive augmentations'.format(num, num_positive - num_negative))
+    else:
+        print('Batch {}: balanced!'.format(num))
